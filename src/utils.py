1bfe2e78650261c2943b9cad9ce2ccff2c283032
exopyl <88846154+exopyl@users.noreply.github.com>
2022-04-12 12:11:22 +0200

    try except

--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,15 @@ def fdp_request(url):
     load_dotenv()
     fdp_url = os.getenv("FDP_URL")
 
-    request = urllib.request.Request(fdp_url+'/'+url)
-    request.add_header("User-Agent", "cheese")
-    response = urllib.request.urlopen(request).read()
-    response_json = json.loads(response)
+    try:
+        request = urllib.request.Request(fdp_url+'/'+url)
+        request.add_header("User-Agent", "cheese")
+        response = urllib.request.urlopen(request).read()
+        response_json = json.loads(response)
+    except:
+        reason = "exception when requesting {}".format(fdp_url+'/'+url)
+        response_json = {"status":"ko", "reason":reason}
+    
     return response_json
+
+
