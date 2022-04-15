ce58bb790fdc326d0a930f32fc380f3da22681a3
exopyl <88846154+exopyl@users.noreply.github.com>
2022-04-15 12:01:41 +0200

    use options

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from src import rtdp_tv,broker,crag
 import pandas as pd
 
-if __name__ == '__main__':
-    print('CRAG')
+_usage_str = """
+Options:
+    [--record <csvfile>, --run <botname>]
+"""
 
+def _usage():
+    print(_usage_str)
 
+def crag_record(csvfile):
+    rtdp = rtdp_tv.RTDPTradingView()
+    rtdp.record(20, 1, csvfile)
+
+def crag_run(botname):
     params = {'infile':'history.csv'}
     rtdp = rtdp_tv.RTDPTradingView(params)
-    #rtdp.record(3, 1, "history.csv")
-    #exit(0)
-    #selection = rtdp_tv.next()
-    #print(selection)
 
     broker = broker.BrokerSimulation()
     broker.initialize({'cash':100})
@@ -20,16 +25,18 @@ if __name__ == '__main__':
     bot.run()
     bot.export_history("broker_history.csv")
     print(bot.broker.get_cash())
-    exit(0)
 
-    for i in range(5):
-        rtdp.next()
-        json_data = rtdp.get_current_data()
-        if json_data is None:
-            continue
-        df_portfolio = pd.read_json(json_data["portfolio"])
-        if isinstance(df_portfolio, pd.DataFrame):
-            print(df_portfolio)
-            print(df_portfolio['symbol'].to_list())
-        json_symbols = json_data["symbols"]
-        print(json_symbols.keys())
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) >= 2:
+        if len(sys.argv) == 3 and (sys.argv[1] == "--record"):
+            csvfile = sys.argv[2]
+            crag_record(csvfile)
+        elif len(sys.argv) >= 2 and (sys.argv[1] == "--run"):
+            if len(sys.argv) == 3:
+                botname = sys.argv[2]
+            crag_run(botname)
+        else:
+            _usage()
+    else:
+        _usage()
