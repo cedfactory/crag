78e777b05b255375e17aa4a061e4562136378b2d
exopyl <88846154+exopyl@users.noreply.github.com>
2022-04-27 00:16:54 +0200

    Update broker_ftx.py

--- src/broker_ftx.py
+++ src/broker_ftx.py
@@ -1,3 +1,8 @@
+'''
+- intégrer fud / ftx / spot_ftx
+- externaliser la stratégie (d'achat et de vente)
+'''
+
 from . import broker
 import ccxt
 from dotenv import load_dotenv
@@ -17,13 +22,20 @@ class BrokerFTX(broker.Broker):
             'apiKey': ftx_api_key,
             'secret': ftx_api_secret
             })
-        return self.ftx_exchange is not None
+        self.authenficated = self.ftx_exchange is not None
+        return self.authenficated
 
-        my_positions = ftx_exchange.fetch_positions()
-        print(my_positions)
-        #my_trades = ftx_exchange.fetch_my_trades()
-        #print(my_trades)
+    def authentication_required(fn):
+        """decoration for methods that require authentification"""
+        def wrapped(self, *args, **kwargs):
+            if not self.ftx_exchange:
+                print("You must be authenticated to use this method {}".format(fn))
+                exit()
+            else:
+                return fn(self, *args, **kwargs)
+        return wrapped
 
+    @authentication_required
     def get_balance(self):
         if self.ftx_exchange:
             balance = self.ftx_exchange.fetch_balance()
@@ -31,20 +43,39 @@ class BrokerFTX(broker.Broker):
             return balance["info"]["result"][0]["free"]
         return 0.
     
+    @authentication_required
     def get_positions(self):
         if self.ftx_exchange:
             positions = self.ftx_exchange.fetch_positions()
             return positions
         return []
        
-
+    @authentication_required
     def get_commission(self, symbol):
         return 0.07
 
+    @authentication_required
     def execute_trade(self, trade):
-        return True
+        if self.ftx_exchange:
+            side = ""
+            if trade.type == "SELL":
+                side = "sell"
+            elif trade.type == "BUY":
+                side = "buy"
+            if side == "":
+                return False
 
+            symbol = trade.symbol
+            amount = trade.net_price
+            order_structure = self.ftx_exchange.create_order(symbol, type, side, amount)
+            print(order_structure)
+            return True
+        return False
+
+    @authentication_required
     def export_history(self, target):
+        my_trades = self.ftx_exchange.fetch_my_trades()
+        print(my_trades)
         pass
 
     def export_status(self):
