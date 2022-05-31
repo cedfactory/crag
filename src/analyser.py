e2f309d78e98f665d50698ed1d4ea471c9fbccf1
Extracheesy <78427936+Extracheesy@users.noreply.github.com>
2022-05-31 15:33:49 +0200

    ___

--- src/analyser.py
+++ src/analyser.py
@@ -28,8 +28,17 @@ class Analyser:
     def __init__(self, params = None):
         self.path = './output/'
 
+        self.df_analyser_result = pd.DataFrame(columns=self.get_df_result_header())
+
         self.df_transaction_records = pd.read_csv(self.path + 'sim_broker_history.csv', delimiter=';')
         self.df_wallet_records = pd.read_csv(self.path + 'wallet_tracking_records.csv')
+
+        self.df_trades = self.df_transaction_records.copy()
+        self.df_trades.drop(self.df_trades[self.df_trades['type'] == 'SOLD'].index, inplace=True)
+        self.df_trades['transaction_roi$'] = self.df_trades['net_price'] \
+                                             - self.df_trades['buying_fees'] - self.df_trades['selling_fees'] \
+                                             - self.df_trades['net_size'] * self.df_trades['buying_price']
+        # self.df_trades.sort_values(by=['transaction_roi%'], ascending=False, inplace=True)
 
         self.starting_time = self.df_wallet_records['time'][0]
         self.ending_time = self.df_wallet_records['time'][len(self.df_wallet_records) - 3] # Foced sell need to provide time... to do list
@@ -37,15 +46,6 @@ class Analyser:
         self.final_wallet = round(self.df_wallet_records['cash'][len(self.df_wallet_records) - 1], 2)
 
         self.performance = round(self.df_wallet_records['roi%'][len(self.df_wallet_records) - 1], 2)
-
-        self.df_trades = self.df_transaction_records.copy()
-        self.df_trades.drop(self.df_trades[self.df_trades['type'] == 'SOLD'].index, inplace=True)
-
-        self.df_trades['transaction_roi$'] = self.df_trades['net_price'] \
-                                             - self.df_trades['buying_fees'] - self.df_trades['selling_fees'] \
-                                             - self.df_trades['net_size'] * self.df_trades['buying_price']
-
-        self.df_trades.sort_values(by=['transaction_roi%'], ascending=False, inplace=True)
 
         self.nb_transaction = len(self.df_trades)
         self.positive_trades = round((self.df_trades['transaction_roi%'] >= 0).sum() * 100 / self.nb_transaction, 2)
@@ -60,11 +60,8 @@ class Analyser:
         self.worst_transaction_val = round(self.df_trades['transaction_roi$'].min(), 2)
         self.mean_transaction_val = round(self.df_trades['transaction_roi$'].mean(), 2)
 
-
         self.list_symbols = list(set(self.df_transaction_records['symbol'].to_list()))
         self.nb_symbols = len(self.list_symbols)
-
-
 
     def display_analyse(self):
         print('period: [', self.starting_time,'] -> [', self.ending_time,']')
@@ -82,9 +79,58 @@ class Analyser:
         print('symbols traded: ', self.list_symbols)
         print('symbols traded nbr: ', self.nb_symbols)
 
+    def set_data_analysed(self):
+        self.list_symbols.append("global")
+
+        for symbol in self.list_symbols:
+            df_symbol_trades = self.df_trades.copy()
+
+            if(symbol != "global"):
+                df_symbol_trades.drop(df_symbol_trades[self.df_trades['symbol'] != symbol].index, inplace=True)
+
+            df_symbol_trades.sort_values(by=['buying_time'], ascending=True, inplace=True)
+            df_symbol_trades.reset_index(drop=True, inplace=True)
+            first_transaction_buying = df_symbol_trades['buying_time'][0]
+
+            df_symbol_trades.sort_values(by=['time'], ascending=False, inplace=True)
+            df_symbol_trades.reset_index(drop=True, inplace=True)
+            last_transaction_selling = df_symbol_trades['time'][0]
+
+            nb_trades_performed = len(df_symbol_trades)
+            win_rate = round((df_symbol_trades['transaction_roi%'] >= 0).sum() * 100 / nb_trades_performed, 2)
+
+            performance_symbol = df_symbol_trades['transaction_roi%'].sum()
+            performance_average_symbol = df_symbol_trades['transaction_roi%'].mean()
+            best_trade_symbol = df_symbol_trades['transaction_roi%'].max()
+            worst_trade_symbol = df_symbol_trades['transaction_roi%'].min()
+
+            performance_value_symbol = df_symbol_trades['transaction_roi$'].sum()
+            performance_value_average_symbol = df_symbol_trades['transaction_roi$'].mean()
+            best_trade_value_symbol = df_symbol_trades['transaction_roi$'].max()
+            worst_trade_value_symbol = df_symbol_trades['transaction_roi$'].min()
+
+            df_new_line = pd.DataFrame([[symbol,
+                                         first_transaction_buying, last_transaction_selling, nb_trades_performed, win_rate,
+                                         performance_symbol, performance_average_symbol, best_trade_symbol, worst_trade_symbol,
+                                         performance_value_symbol, performance_value_average_symbol, best_trade_value_symbol, worst_trade_value_symbol]],
+                                       columns=self.get_df_result_header())
+
+            self.df_analyser_result = pd.concat([self.df_analyser_result, df_new_line])
+            self.df_analyser_result.reset_index(inplace=True, drop=True)
+
+
+        self.df_analyser_result.to_csv(self.path + 'analyser_records.csv')
+
+
+    def get_df_result_header(self):
+        return ["symbol",
+                "first_buying", "last_selling", "trades_performed", "win_rate",
+                "performance%", "performance_average%", "best_trade%", "worst_trade%",
+                "performance_value$", "performance_value_average$", "best_trade_value$", "worst_trade_value$"]
 
 
     def run_analyse(self):
+        self.set_data_analysed()
         self.display_analyse()
 
 
