2ce308d2ba1f86882cc43c9e3395761a2f192f74
Extracheesy <78427936+Extracheesy@users.noreply.github.com>
2022-06-12 13:28:13 +0200

    ___

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src import rtdp,rtdp_simulation,broker_simulation,broker_ftx,crag,rtstr_super_reversal,rtstr_trix,rtstr_cryptobot,analyser,benchmark
+from src import rtdp,rtdp_simulation,broker_simulation,broker_ftx,crag,rtstr_super_reversal,rtstr_trix,rtstr_cryptobot,rtstr_bigwill,analyser,benchmark
 import pandas as pd
 
 _usage_str = """
@@ -22,6 +22,8 @@ def crag_simulation(strategy_name):
         strategy = rtstr_trix.StrategyTrix(params={"rtctrl_verbose": False})
     if strategy_name == "cryptobot":
         strategy = rtstr_cryptobot.StrategyCryptobot(params={"rtctrl_verbose": False})
+    if strategy_name == "bigwill":
+        strategy = rtstr_bigwill.StrategyBigWill(params={"rtctrl_verbose": False})
 
     broker_params = {'cash':10000}
     simu_broker = broker_simulation.SimBroker(broker_params)
@@ -90,7 +92,7 @@ if __name__ == '__main__':
         if len(sys.argv) == 2 and (sys.argv[1] == "--record"):
             crag_record()
         elif len(sys.argv) == 2 and (sys.argv[1] == "--simulation"):
-            crag_simulation('super_reversal')
+            crag_simulation('bigwill')
         elif len(sys.argv) >= 2 and (sys.argv[1] == "--run"):
             strategy_name = ""
             if len(sys.argv) >= 3:
