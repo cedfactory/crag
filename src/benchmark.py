90d0d05ec36b93950494e9046b34b3207470daab
Extracheesy <desportes.cedric@gmail.com>
2022-06-13 02:34:54 +0200

    ___
    Signed-off-by: exopyl <exopyl74@mail.ru>

--- src/benchmark.py
+++ src/benchmark.py
@@ -10,8 +10,12 @@ import os, fnmatch
 class Benchmark:
     def __init__(self, params = None):
         self.path = './benchmark/'
+        self.path_output = self.path + '/output/'
         self.path_symbol_data = './data_processed/'
         self.path_symbol_plot_analyse = './benchmark/plot_analyse/'
+
+        if not os.path.exists(self.path_output):
+            os.makedirs(self.path_output)
 
         self.df_transaction_records = pd.DataFrame()
         self.df_wallet_records = pd.DataFrame()
@@ -35,8 +39,16 @@ class Benchmark:
             elif prefixe.split("_")[0] == 'wallet':
                 if len(self.df_wallet_records) == 0:
                     self.df_wallet_records = df_data.copy()
+                    start = self.df_wallet_records['time'][0]
+                    start = start.split(" ")[0]
+                    end = self.df_wallet_records['time'][len(self.df_wallet_records)-3] # CEDE DEBUG -3
+                    end = end.split(" ")[0]
                 else:
                     self.df_wallet_records = pd.concat([self.df_wallet_records, df_data])
+        self.path_results = self.path_output + '/' + start + '_to_' + end + '/'
+        if not os.path.exists(self.path_results):
+            os.makedirs(self.path_results)
+        period = " from_" + start + ' to ' + end
 
         self.list_strategies = list(set(self.list_strategies))
 
@@ -58,40 +70,91 @@ class Benchmark:
         ax = plt.gca()
         for strategy in self.list_strategies:
             self.df_wallet.plot(kind='line', y=strategy, ax=ax)
-        plt.savefig(self.path + 'wallet_strategy.png')
+        # ax.legend(bbox_to_anchor=(1, 1.02), loc='upper left')
+        plt.title('wallet_strategy' + period)
+        plt.savefig(self.path_results + 'wallet_strategy.png')
         plt.clf()
 
         # Global Win rate
-        self.df_win_rate = pd.DataFrame()
+        self.df_plot_data = pd.DataFrame()
         for strategy in self.list_strategies:
             df_strategy = self.df_transaction_records[self.df_transaction_records['strategy'] == strategy].copy()
             df_strategy = df_strategy[df_strategy['type'] == 'SELL'].copy()
-            if len(self.df_win_rate) == 0:
-                self.df_win_rate = pd.DataFrame(columns=self.list_strategies, index=['transaction_total', 'global_win_rate'])
+            if len(self.df_plot_data) == 0:
+                self.df_plot_data = pd.DataFrame(columns=self.list_strategies, index=['transaction_total',
+                                                                                      'global_win_rate',
+                                                                                      'profit$',
+                                                                                      'profit%'])
 
-            self.df_win_rate[strategy]['transaction_total'] = len(df_strategy)
+            self.df_plot_data[strategy]['transaction_total'] = len(df_strategy)
 
             # df_strategy['transaction_roi$'] = df_strategy['net_price'] - df_strategy['buying_fees'] - df_strategy['selling_fees'] - df_strategy['net_size'] * df_strategy['buying_price']
             df_strategy['transaction_roi$'] = df_strategy['net_price'] - df_strategy['buying_fees'] - df_strategy['net_size'] * df_strategy['buying_price']
 
-            self.df_win_rate[strategy]['global_win_rate'] = round((df_strategy['transaction_roi$'] >= 0).sum() * 100 / self.df_win_rate[strategy]['transaction_total'], 2)
+            self.df_plot_data[strategy]['global_win_rate'] = round((df_strategy['transaction_roi$'] >= 0).sum() * 100 / self.df_plot_data[strategy]['transaction_total'], 2)
 
-        df_win_rate = self.df_win_rate.copy()
-        df_win_rate.drop(inplace=True, index='transaction_total')
-        df_win_rate.reset_index(inplace=True)
+            df_strategy = self.df_wallet_records[self.df_wallet_records['strategy'] == strategy].copy()
+            self.df_plot_data[strategy]['profit$'] = round(df_strategy['wallet'][len(df_strategy)-1] - df_strategy['wallet'][0], 1)
+            self.df_plot_data[strategy]['profit%'] = round(self.df_plot_data[strategy]['profit$'] * 100 / df_strategy['wallet'][0], 1)
+
         # Plot win rate
+        df_win_rate = self.df_plot_data.copy()
+        for row_to_drop in df_win_rate.index.to_list():
+            if (row_to_drop != 'global_win_rate'):
+                df_win_rate.drop(inplace=True, index=row_to_drop)
+        df_win_rate.reset_index(inplace=True)
+
         ax = plt.gca()
         df_win_rate.plot(kind='bar', x='index', ax=ax)
-        plt.savefig(self.path + 'win_rate.png')
+        for i in range(len(ax.containers)):
+            ax.bar_label(ax.containers[i])
+        plt.title('win_rate' + period)
+        plt.savefig(self.path_results + 'win_rate.png')
         plt.clf()
 
-        df_transaction_total = self.df_win_rate.copy()
-        df_transaction_total.drop(inplace=True, index='global_win_rate')
-        df_transaction_total.reset_index(inplace=True)
         # Plot win rate
+        df_transaction_total = self.df_plot_data.copy()
+        for row_to_drop in df_transaction_total.index.to_list():
+            if (row_to_drop != 'transaction_total'):
+                df_transaction_total.drop(inplace=True, index=row_to_drop)
+        df_transaction_total.reset_index(inplace=True)
+
         ax = plt.gca()
         df_transaction_total.plot(kind='bar', x='index', ax=ax)
-        plt.savefig(self.path + 'transaction_total.png')
+        for i in range(len(ax.containers)):
+            ax.bar_label(ax.containers[i])
+        plt.title('total_transaction_performed' + period)
+        plt.savefig(self.path_results + 'transaction_total.png')
+        plt.clf()
+
+        # Plot profit $
+        df_profit = self.df_plot_data.copy()
+        for row_to_drop in df_profit.index.to_list():
+            if (row_to_drop != 'profit$'):
+                df_profit.drop(inplace=True, index=row_to_drop)
+        df_profit.reset_index(inplace=True)
+
+        ax = plt.gca()
+        df_profit.plot(kind='bar', x='index', ax=ax)
+        for i in range(len(ax.containers)):
+            ax.bar_label(ax.containers[i])
+        plt.title('profit $' + period)
+        plt.savefig(self.path_results + 'profit$.png')
+        plt.clf()
+
+        # Plot profit %
+        df_profit = self.df_plot_data.copy()
+        for row_to_drop in df_profit.index.to_list():
+            if (row_to_drop != 'profit%'):
+                df_profit.drop(inplace=True, index=row_to_drop)
+        df_profit.reset_index(inplace=True)
+
+        ax = plt.gca()
+        df_profit.plot(kind='bar', x='index', ax=ax)
+        for i in range(len(ax.containers)):
+            ax.bar_label(ax.containers[i])
+        plt.title('profit %' + period)
+        plt.savefig(self.path_results + 'profit%.png')
         plt.clf()
 
     def run_benchmark(self):
