0b0752dca4263997636dca99f4253ad1a4067733
exopyl <88846154+exopyl@users.noreply.github.com>
2022-07-20 15:45:58 +0200

    expose get_current_trades_sample function

--- test/test_rtctrl.py
+++ test/test_rtctrl.py
@@ -1,31 +1,30 @@
 import pytest
 from src import trade,rtctrl
 
+def get_current_trades_sample():
+    current_trades_content = [
+        {'type':'SELL', 'symbol':'symbol1', 'size':1, 'buying_fee':0.1, 'gross_price':10},
+        {'type':'BUY', 'symbol':'symbol2', 'size':2, 'buying_fee':0.2, 'gross_price':20},
+        {'type':'BUY', 'symbol':'symbol2', 'size':3, 'buying_fee':0.3, 'gross_price':30},
+        {'type':'BUY', 'symbol':'symbol3', 'size':4, 'buying_fee':0.4, 'gross_price':40}
+        ]
+    current_trades = []
+    for content in current_trades_content:
+        current_trade = trade.Trade()
+        current_trade.type = content["type"]
+        current_trade.symbol = content["symbol"]
+        current_trade.net_size = content["size"]
+        current_trade.buying_fee = content["buying_fee"]
+        current_trade.gross_price = content["gross_price"]
+        current_trades.append(current_trade)
+
+    return current_trades
+
 class TestRTCTRL:
-
-    def get_current_trades_for_get_functions(self):
-        current_trades_content = [
-            {'type':'SELL', 'symbol':'symbol1', 'size':1, 'buying_fee':0.1, 'gross_price':10},
-            {'type':'BUY', 'symbol':'symbol2', 'size':2, 'buying_fee':0.2, 'gross_price':20},
-            {'type':'BUY', 'symbol':'symbol2', 'size':3, 'buying_fee':0.3, 'gross_price':30},
-            {'type':'BUY', 'symbol':'symbol3', 'size':4, 'buying_fee':0.4, 'gross_price':40}
-            ]
-        current_trades = []
-        for content in current_trades_content:
-            current_trade = trade.Trade()
-            current_trade.type = content["type"]
-            current_trade.symbol = content["symbol"]
-            current_trade.net_size = content["size"]
-            current_trade.buying_fee = content["buying_fee"]
-            current_trade.gross_price = content["gross_price"]
-            current_trades.append(current_trade)
-        
-        return current_trades
-
 
     def test_get_list_of_traded_symbols(self):
         # context
-        current_trades = self.get_current_trades_for_get_functions()
+        current_trades = get_current_trades_sample()
         ctrl = rtctrl.rtctrl()
 
         # action
@@ -38,7 +37,7 @@ class TestRTCTRL:
 
     def test_get_list_of_asset_size(self):
         # context
-        current_trades = self.get_current_trades_for_get_functions()
+        current_trades = get_current_trades_sample()
         ctrl = rtctrl.rtctrl()
         ctrl.symbols = ['symbol2', 'symbol3']
 
@@ -51,7 +50,7 @@ class TestRTCTRL:
 
     def test_get_list_of_asset_fees(self):
         # context
-        current_trades = self.get_current_trades_for_get_functions()
+        current_trades = get_current_trades_sample()
         ctrl = rtctrl.rtctrl()
         ctrl.symbols = ['symbol2', 'symbol3']
 
@@ -64,7 +63,7 @@ class TestRTCTRL:
 
     def test_get_list_of_asset_gross_price(self):
         # context
-        current_trades = self.get_current_trades_for_get_functions()
+        current_trades = get_current_trades_sample()
         ctrl = rtctrl.rtctrl()
         ctrl.symbols = ['symbol2', 'symbol3']
 
@@ -77,7 +76,7 @@ class TestRTCTRL:
 
     def test_update(self, mocker):
         # context
-        current_trades = self.get_current_trades_for_get_functions()
+        current_trades = get_current_trades_sample()
         ctrl = rtctrl.rtctrl()
 
         # action
