d4a3b7b7eef206b14291a74f85930156fab12f3d
exopyl <88846154+exopyl@users.noreply.github.com>
2022-07-20 17:13:53 +0200

    remove duplication

--- src/rtstr_VMC.py
+++ src/rtstr_VMC.py
@@ -93,7 +93,3 @@ class StrategyVMC(rtstr.RealTimeStrategy):
 
         df_result = pd.DataFrame(data)
         return df_result
-
-    def update(self, current_datetime, current_trades, broker_cash, prices_symbols):
-        self.rtctrl.update_rtctrl(current_datetime, current_trades, broker_cash, prices_symbols)
-        self.rtctrl.display_summary_info()
