1e8c3a8cd1b81e81fdf0f13723c2d97ec03cb68f
exopyl <exopyl74@mail.ru>
2022-08-06 00:23:04 +0200

    fix

--- test/test_utils.py
+++ test/test_utils.py
@@ -16,7 +16,7 @@ class TestUtils:
 
     def test_fdp_request_ko_unknown_service(self, mocker):
         # context
-        mocker.patch('os.getenv', side_effect=["fake_fdp_url"])
+        mocker.patch('os.getenv', side_effect=["fake_fdp_url/"])
 
         # action
         response_json = utils.fdp_request({"service":"fake_service"})
@@ -27,11 +27,12 @@ class TestUtils:
 
     def test_fdp_request_ko_bad_fdp_url(self, mocker):
         # context
-        mocker.patch('os.getenv', side_effect=["fake_fdp_url"])
+        mocker.patch('os.getenv', side_effect=["fake_fdp_url/"])
 
         # action
         response_json = utils.fdp_request({"service":"history", "exchange":"ftx", "symbol":"BTC_USD", "start":"2022-01-01", "end": "2022-02-01", "interval": "1h"})
 
         # expectations
         assert(response_json["status"] == "ko")
+        print(response_json["info"])
         assert(response_json["info"] == "exception when requesting fake_fdp_url/history?exchange=ftx&symbol=BTC_USD&start=2022-01-01&interval=1h&end=2022-02-01")
