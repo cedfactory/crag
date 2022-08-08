7e0283bd8ef5c52569fcb7797d780d3094ad9e72
exopyl <exopyl74@mail.ru>
2022-08-08 20:26:15 +0200

    fix

--- test/test_rtctrl.py
+++ test/test_rtctrl.py
@@ -82,7 +82,7 @@ class TestRTCTRL:
         # action
         prices_symbols = {'symbol1': 0.01, 'symbol2': 0.02, 'symbol3': 0.03, 'symbol4': 0.04}
         current_datetime = "2022-04-01"
-        ctrl.update_rtctrl(current_datetime, current_trades, 100, prices_symbols)
+        ctrl.update_rtctrl(current_datetime, current_trades, 100, prices_symbols, None)
 
         # expectations
         df = ctrl.df_rtctrl
--- test/test_rtstr.py
+++ test/test_rtstr.py
@@ -9,7 +9,7 @@ def update_rtctrl(rtstr):
     # action
     prices_symbols = {'symbol1': 0.01, 'symbol2': 0.02, 'symbol3': 0.03, 'symbol4': 0.04}
     current_datetime = "2022-04-01"
-    rtstr.rtctrl.update_rtctrl(current_datetime, current_trades, 100, prices_symbols)
+    rtstr.rtctrl.update_rtctrl(current_datetime, current_trades, 100, prices_symbols, None)
 
     return rtstr
 
