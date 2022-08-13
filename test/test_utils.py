1970d7bedd270ebeb8ebf7fa48f4ce5ece471899
exopyl <exopyl74@mail.ru>
2022-08-13 14:35:49 +0200

    fix

--- test/test_utils.py
+++ test/test_utils.py
@@ -30,7 +30,7 @@ class TestUtils:
         mocker.patch('os.getenv', side_effect=["fake_fdp_url/"])
 
         # action
-        response_json = utils.fdp_request({"service":"history", "exchange":"ftx", "symbol":"BTC_USD", "start":"2022-01-01", "end": "2022-02-01", "interval": "1h"})
+        response_json = utils.fdp_request({"service":"history", "exchange":"ftx", "symbol":"BTC_USD", "start":"2022-01-01", "end": "2022-02-01", "interval": "1h"}, False)
 
         # expectations
         assert(response_json["status"] == "ko")
