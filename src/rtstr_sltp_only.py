f343c0726856c1e1a277f7e0974814fa59b28bdb
Extracheesy <desportes.cedric@gmail.com>
2023-10-18 21:45:37 +0200

    sltp

---
+++ src/rtstr_sltp_only.py
@@ -0,0 +1,46 @@
+from . import rtdp, rtstr, rtctrl
+import pandas as pd
+
+class StrategySLTPOnly(rtstr.RealTimeStrategy):
+
+    def __init__(self, params=None):
+        super().__init__(params)
+
+        self.rtctrl = rtctrl.rtctrl(params=params)
+        self.rtctrl.set_list_open_position_type(self.get_lst_opening_type())
+        self.rtctrl.set_list_close_position_type(self.get_lst_closing_type())
+
+        self.zero_print = True
+
+    def get_data_description(self):
+        ds = rtdp.DataDescription()
+        ds.symbols = self.lst_symbols
+
+        ds.fdp_features = { "ema10" : {"indicator": "ema", "id": "10", "window_size": 10}
+                            }
+
+        ds.features = self.get_feature_from_fdp_features(ds.fdp_features)
+        ds.interval = self.strategy_interval
+        print("startegy: ", self.get_info())
+        print("strategy features: ", ds.features)
+
+        return ds
+
+    def get_info(self):
+        return "StrategySLTPOnly"
+
+    def condition_for_opening_long_position(self, symbol):
+        return False
+
+    def condition_for_opening_short_position(self, symbol):
+        return False
+
+    def condition_for_closing_long_position(self, symbol):
+        return False
+
+    def condition_for_closing_short_position(self, symbol):
+        return False
+
+    def sort_list_symbols(self, lst_symbols):
+        print("symbol list: ", lst_symbols)
+        return lst_symbols
