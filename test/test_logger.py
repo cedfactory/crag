f9a87ea26603623d3792882ac2e42fb5b3f32da4
exopyl <exopyl74@mail.ru>
2024-04-01 21:34:41 +0200

    fix test

--- test/test_logger.py
+++ test/test_logger.py
@@ -51,7 +51,7 @@ class TestLoggerFile:
         # expectations
         assert (os.path.isfile(expected_filename))
         current_filesize = my_logger._get_current_filesize()
-        assert (current_filesize == 6)
+        assert (current_filesize == 35)
         with open(expected_filename) as f:
             lines = f.readlines()
             assert (len(lines) == 1)
@@ -83,7 +83,7 @@ class TestLoggerFile:
         assert (os.path.isfile(expected_filename0))
         assert (os.path.isfile(expected_filename1))
         current_filesize = my_logger._get_current_filesize()
-        assert (current_filesize == 11)
+        assert (current_filesize == 40)
         with open(expected_filename0) as f:
             lines = f.readlines()
             assert (len(lines) == 1)
