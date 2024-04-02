5424eeb41bbff93098e9dc8771c6702249b66840
exopyl <exopyl74@mail.ru>
2024-04-02 19:13:56 +0200

    fix conversion string to boleean

--- src/broker_bitget.py
+++ src/broker_bitget.py
@@ -14,6 +14,7 @@ class BrokerBitGet(broker.Broker):
         self.zero_print = True
         if params:
             self.zero_print = params.get("zero_print", self.zero_print)
+            self.zero_print = self.zero_print == "True" # convert to boolean
             self.simulation = params.get("simulation", self.simulation)
             if self.simulation == 0 or self.simulation == "0":
                 self.simulation = False
