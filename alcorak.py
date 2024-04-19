71575e859d455927161fd05b1b348dbfc21007d7
exopyl <exopyl74@mail.ru>
2024-04-19 23:01:25 +0200

    :rocket: alcorak

---
+++ alcorak.py
@@ -0,0 +1,79 @@
+import subprocess
+import os
+import sys
+from src import crag_helper,broker_bitget_api
+from rich import print
+import platform
+
+g_os_platform = platform.system()
+g_python_executable = ""
+
+def start_strategy(strategy_configuration_file):
+
+    print("Launching {}...".format(strategy_configuration_file))
+    configuration = crag_helper.load_configuration_file(strategy_configuration_file)
+    if not configuration:
+        print("💥 A problem occurred while loading {}".format(strategy_configuration_file))
+        return
+
+    #result = subprocess.run(['cmd.exe', '/c', 'dir'], capture_output=True, text=True)
+    result = subprocess.run([g_python_executable, "main.py", "--live", strategy_configuration_file], stdout=subprocess.PIPE)
+    print(result)
+
+    backup_file = "./output/grid_crag_backup.pickle"
+    while result.returncode == 1 and os.path.exists(backup_file):
+        print("size of {} : {} bytes".format(backup_file, os.path.getsize(backup_file)))
+        result = subprocess.run(
+            [g_python_executable, "main.py", "--reboot", backup_file],
+            stdout=subprocess.PIPE)
+        print(result)
+
+        # filtrer le slogs de ced
+
+
+    params_broker = configuration["broker"]
+    my_broker = broker_bitget_api.BrokerBitGetApi(params_broker)
+    my_broker.execute_reset_account()
+    my_broker.cancel_all_orders(["XRP"])
+
+
+def stop_strategy(strategy_config_file):
+    print("Stopping {}... TODO !!!".format(strategy_config_file))
+
+
+_usage_str = '''
+Options:
+    --start <StrategyConfigFile>
+    --stop <StrategyConfigFile>
+'''
+
+def _usage():
+    print(_usage_str)
+
+if __name__ == '__main__':
+    print('''
+       ______   __                                          __       
+      /      \ |  \                                        |  \      
+     |  $$$$$$\| $$  _______   ______    ______    ______  | $$   __ 
+     | $$__| $$| $$ /       \ /      \  /      \  |      \ | $$  /  \\
+     | $$    $$| $$|  $$$$$$$|  $$$$$$\|  $$$$$$\  \$$$$$$\| $$_/  $$
+     | $$$$$$$$| $$| $$      | $$  | $$| $$   \$$ /      $$| $$   $$ 
+     | $$  | $$| $$| $$_____ | $$__/ $$| $$      |  $$$$$$$| $$$$$$\ 
+     | $$  | $$| $$ \$$     \ \$$    $$| $$       \$$    $$| $$  \$$\\
+      \$$   \$$ \$$  \$$$$$$$  \$$$$$$  \$$        \$$$$$$$ \$$   \$$
+     ''')
+    print("Platform :", g_os_platform)
+    g_python_executable = sys.executable
+    print("Python executable :", g_python_executable)
+
+    if len(sys.argv) > 1 and (sys.argv[1] == "--start"):
+        strategy_configuration_file = sys.argv[2]
+        start_strategy(strategy_configuration_file)
+        print("launching {}...".format(strategy_configuration_file))
+    elif len(sys.argv) > 1 and (sys.argv[1] == "--stop"):
+        strategy_configuration_file = sys.argv[2]
+        stop_strategy(strategy_configuration_file)
+
+
+    else:
+        _usage()
