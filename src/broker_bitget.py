4e9eaf5e22f42f1715999413b5bacd78793ea287
Extracheesy <desportes.cedric@gmail.com>
2024-10-27 11:54:25 +0100

    minor fix for discord traces

--- src/broker_bitget.py
+++ src/broker_bitget.py
@@ -1008,10 +1008,11 @@ class BrokerBitGet(broker.Broker):
             del condition
             return grid_id
         else:
-            msg = "ERROR: BROKER ID NOT FOUND" + "\n"
-            msg += "ORDER ID: " + str(orderId) + "\n"
-            self.log_trade = self.log_trade + msg.upper()
-            del msg
+            # CEDE USED FOR DEBUG
+            # msg = "ERROR: BROKER ID NOT FOUND" + "\n"
+            # msg += "ORDER ID: " + str(orderId) + "\n"
+            # self.log_trade = self.log_trade + msg.upper()
+            # del msg
             del condition
             return None
 
