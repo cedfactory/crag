68c49b445dc6fff76dfafe82c1611252db33d72f
exopyl <exopyl74@mail.ru>
2024-11-24 09:39:58 +0100

    fix tests

--- test/utils.py
+++ test/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 
 def write_file(filename, string):
@@ -6,12 +7,26 @@ def write_file(filename, string):
         f.write(string)
     return filename
 
+
 def get_json_for_get_df_range():
     df = pd.read_csv("./test/data/AAVE_USD.csv", delimiter=',')
     df.drop(["Unnamed: 0"], axis=1, inplace=True)
-    df['timestamp'] = df['timestamp'].astype('string') # convert object type to string type
-    df['timestamp'] = pd.to_datetime(df['timestamp'], format="%d/%m/%Y %H:%M") # format datetime
-    df.rename(columns = {"timestamp":"index"}, inplace = True)
-    json_df = {'result': {'BTC_USDT': {'status': 'ok', 'info':df.to_json()}}}
+    df['timestamp'] = df['timestamp'].astype('string')  # convert object type to string type
+    df['timestamp'] = pd.to_datetime(df['timestamp'], format="%d/%m/%Y %H:%M")  # format datetime
+    df.rename(columns={"timestamp": "index"}, inplace=True)
+    json_df = {'result': {'BTC_USDT': {'status': 'ok', 'info': df.to_json()}}}
 
     return json_df
+
+
+def detect_environment():
+    if os.environ.get("GITHUB_ACTIONS") == "true":
+        return "github"
+    elif os.environ.get("CI") == "true":
+        return "gitlab"
+    elif os.environ.get("TRAVIS") == "true":
+        return "travis"
+    elif os.environ.get("JENKINS_URL"):
+        return "jenkins"
+    else:
+        return "local"
